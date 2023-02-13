40db4ad59f3ecd0eb1d77c7172d232e1ba75e6ad
Gustavo <elmergustavo79@gmail.com>
2023-02-13 14:24:49 -0600

    add install taildwind css

---
+++ app/Http/Livewire/SendGmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SendGmail extends Component
+{
+    public function render()
+    {
+        return view('livewire.send-gmail');
+    }
+}
---
+++ resources/views/livewire/send-gmail.blade.php
@@ -0,0 +1,12 @@
+<div>
+  {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <label class="text-gray-700 uppercase font-bold text-sm" htmlFor="email">
+    Ingrese un correo
+  </label>
+  <input type="" id="text" placeholder="Email del Usuario"
+    class="bg-black"/>
+</div>
