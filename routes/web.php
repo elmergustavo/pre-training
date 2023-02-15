e752c71781f0b79cfcbbb6e6b9f1ce6bb8037c32
Gustavo <elmergustavo79@gmail.com>
2023-02-15 12:53:56 -0600

    queue work and horizon

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\RedisController;
 use Illuminate\Support\Facades\Route;
 use App\Jobs\sendEmail;
+use App\Models\User;
+use App\Notifications\NotifyUser;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +21,23 @@ use App\Jobs\sendEmail;
 Route::get('/', function () {
     //envio en segundo plano para determinar los cambios 
    // sendEmail::dispatch();
-    sendEmail::dispatchAfterResponse(); // 
-    return response("\nfin");
+    // sendEmail::dispatchAfterResponse(); // 
+    // return response("\nfin");
+
+
+
+     User::find(99)->notify(new NotifyUser());
+     User::find(20)->notify(new NotifyUser());
+     User::find(10)->notify(new NotifyUser());
+     User::find(15)->notify(new NotifyUser());
+     User::find(25)->notify(new NotifyUser());
+  
+
+   
+
+    //   echo  User::find(1);
+    return 'Se ha enviado los correos perfectamente';
+   
 });
 
 Route::get('/main', function () {
